3829f2d7a7deffb1049f86f3c963539489666ec2
Radu Butoi <rbutoi@gmail.com>
2015-01-18 04:22:58 -0500

    more db stuff

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import webapp2
 from mainpage import MainPage
 from receivetext import ReceiveText
 from receivecall import *
-from receivescript import ReceiveScript
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/receivetext', ReceiveText),
     ('/receivecall', ReceiveCall),
-    ('/receivescript', ReceiveScript),
     ('/handle-recording0', ReceiveCall1),
     ('/handle-recording1', ReceiveCall2),
     ('/handle-recording2', ReceiveCall3),
