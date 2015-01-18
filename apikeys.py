4725b3c0a34946aa46a52f591cb9b21750b25b7b
Bazhunga <zhu.s.kevin@gmail.com>
2015-01-18 02:47:15 -0500

    postmate quotes

--- apikeys.py
+++ apikeys.py
@@ -1,2 +1,4 @@
 appID = 'D0zjMY1ObbT95PlKu8lOtHaW1njYovk72MXqeIst'
 restAPIkey = 'TUcZ0FwmgXaypbhnCu4Ir886DwlwPZKGpbWvosPC'
+pm_customerID = 'cus_KAexnF0RCzFpck'
+pm_appID = '25b9b722-0286-4747-aa1b-e369329b82d6'
